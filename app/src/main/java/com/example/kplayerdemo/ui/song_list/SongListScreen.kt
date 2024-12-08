a8d637a86aaabd596d28b12a78b31847f41635ad
gntx <garnet.xiong@gmail.com>
2024-12-08 13:53:59 +0800

    feat: Add message for offline state and no data state

--- app/src/main/java/com/example/kplayerdemo/ui/song_list/SongListScreen.kt
+++ app/src/main/java/com/example/kplayerdemo/ui/song_list/SongListScreen.kt
@@ -1,13 +1,12 @@
 package com.example.kplayerdemo.ui.song_list
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -28,6 +27,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -45,6 +45,10 @@ fun SongListScreen(
 ) {
     val state by viewModel.state.collectAsState()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    var title = "Results for \" ${Constants.DEFAULT_ARTIST_NAME} \""
+    if (state.isLoading) {
+        title = "Fetching results for \" ${Constants.DEFAULT_ARTIST_NAME} \""
+    }
 
     Scaffold(
         modifier = Modifier
@@ -53,8 +57,8 @@ fun SongListScreen(
         topBar = {
             MediumTopAppBar(
                 title = {
-                    Text(Constants.DEFAULT_ARTIST_NAME)
-                },
+                    Text(title)
+                        },
                 scrollBehavior = scrollBehavior
             )
         }) { innerPadding ->
@@ -63,6 +67,18 @@ fun SongListScreen(
             Column(
                 modifier = Modifier.fillMaxSize()
             ) {
+                if (state.error.isNotBlank()) {
+                    Text(
+                        text = "You're offline. Results may be limited.",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.bodyMedium.merge(),
+                        color = Color.White,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.Black)
+                            .padding(horizontal = 20.dp)
+                    )
+                }
                 OutlinedTextField(
                     value = state.queryString,
                     onValueChange = { username -> viewModel.updateQueryString(username) },
@@ -81,12 +97,12 @@ fun SongListScreen(
                         SongListItem(song)
                     }
                 }
-                Spacer(modifier = Modifier.height(30.dp))
+                //Spacer(modifier = Modifier.height(10.dp))
             }
 
-            if (state.error.isNotBlank()) {
+            if (state.displaySongs.isEmpty()) {
                 Text(
-                    text = state.error,
+                    text = "No data",
                     textAlign = TextAlign.Center,
                     modifier = Modifier
                         .fillMaxWidth()
